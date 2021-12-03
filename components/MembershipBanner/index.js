9ec51794d43cde2e8b3c0f1a66b3c24f2220457a
Igor De Latorre <delatorreigor0@gmail.com>
2021-12-03 09:10:53 -0300

    Minor refactor in MembershipBanner

--- components/MembershipBanner/index.js
+++ components/MembershipBanner/index.js
@@ -1,27 +1,24 @@
 import React from "react";
 import style from "./MembershipBanner.module.css"; 
-import Button, { ButtonBase } from "../ButtonBase/index";
+import { ButtonBase } from "../ButtonBase/index";
 import Image from "next/image";
 import nurseImg from "../../public/images/nurse.svg"
 
 const MembershipBanner = () => {
     return(
         <div className={style.banner}>
+            <div>
+                <Image className={style.photo} src={nurseImg} />
+            </div>
             <div className={style.info}>
                 <h1 className={style.textHeader}>Seja um associado da ABEN-RS!</h1>
                 <p className={style.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada lectus eget 
                 risus convallis pulvinar sed consequat scelerisque nibh. Quis etiam felis egestas sapien nec rhoncus, ut 
                 pellentesque.</p>
-                <div>
-                    <ButtonBase color="secondary" onClick={() => console.log("associe-se vc também :)")}>
-                        Associe-se {'\u2794'}
-                    </ButtonBase>
-                </div>
+                <ButtonBase style={{marginLeft:"0px"}}color="secondary" onClick={() => console.log("associe-se vc também :)")}>
+                    Associe-se {'\u2794'}
+                </ButtonBase>
             </div>
-            <div className={style.photo}>
-                <Image src={nurseImg} />
-            </div>
-
         </div>
     );
 };
