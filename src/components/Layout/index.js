ef4efc87c4e7eb6cd0af7d4fe573c64f05952256
Artur <arturwodroid@gmail.com>
2021-12-06 20:49:40 -0300

    fix: Layout positioning

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Box } from '@mui/system';
 
 const Layout = ({
   children,
 }) => {
   return (
-    <>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
+      }}>
       <Header />
-      {children}
+      <Box
+        sx={{
+          minHeight: "100%",
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start"
+        }}>
+        {children}
+      </Box>
       <Footer />
-    </>
+    </Box>
   )
 }
 export default Layout;
