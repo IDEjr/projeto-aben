81829e5d69ccaea60864640dec04a3ffb2eec11b
igordelatorre <delatorreigor0@gmail.com>
2021-12-21 17:27:52 -0300

    Refactor NewsGrid header using mui

--- src/components/NewsGrid/index.js
+++ src/components/NewsGrid/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import style from "./NewsGrid.module.css";
 import News from "../News/index";
-import { ButtonBase } from "../ButtonBase/index";
+import { Button, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 
 const NewsGrid = ({
   renderAllNews = true,
   hasGridHeader = false,
   newsData,
 }) => {
+  const router = useRouter();
+  const handleClick = () => router.push("/Noticias");
+
   const defaultNumberOfNewsToRender = 6;
   const numberOfNewsToRender = renderAllNews
     ? newsData.length
@@ -28,20 +32,18 @@ const NewsGrid = ({
   return (
     <div>
       {hasGridHeader && (
-        <div className={style.gridHeader}>
-          <h1 className={style.text}>Notícias</h1>
-          <ButtonBase
-            style={{
-              marginRight: "2rem",
-              marginLeft: "auto",
-              alignSelf: "flex-end",
-            }}
-            color="light"
-            onClick={() => console.log("apertou no botaoo aee")}
-          >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{ maxWidth: 1100 }}
+        >
+          <Typography variant="h4" sx={{ ml: 1.5 }}>
+            Notícias
+          </Typography>
+          <Button variant="outline" sx={{ mr: 1.5 }} onClick={handleClick}>
             Mais {"\u2794"}
-          </ButtonBase>
-        </div>
+          </Button>
+        </Stack>
       )}
       <div className={style.grid}>{newsToRender}</div>
     </div>
