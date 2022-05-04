bdfc3b348c771a0ea21d8a401bf3c7981b01ae5f
IgorDalpiaz <igor.dalpiaz@networkme.io>
2022-05-04 20:32:57 -0300

    multifixes3

--- src/components/Carroussel/index.js
+++ src/components/Carroussel/index.js
@@ -9,9 +9,10 @@ import "swiper/css/navigation";
 
 // import Swiper core and required modules
 import SwiperCore, { Navigation, Autoplay } from "swiper";
-import { Button, Container, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, CardActionArea, CardMedia, Container, Grid, Typography } from "@mui/material";
+import { Box, color } from "@mui/system";
 import { useRouter } from "next/router";
+
 
 // install Swiper modules
 SwiperCore.use([Navigation, Autoplay]);
@@ -24,30 +25,60 @@ const Carroussel = ({
   const handleClick = () => router.push("/eventos");
 
   return (
-    <Container>
-      <Box
-        display="flex"
-        direction="row"
-        justifyContent="space-between"
-        mb={2}
-      >
-        <Typography variant="h4">
-          Eventos
-        </Typography>
-        <Button variant="outline" onClick={handleClick}>
-          Mais {"\u2794"}
-        </Button>
-      </Box>
-      <CustomCarrousel autoplay>
-        {data.slice(0, 10).map(e => {
-          return (
-            <SwiperSlide key={e.fileName}>
-              <Slide {...e} />
-            </SwiperSlide>
-          );
-        })}
-      </CustomCarrousel>
-    </Container>
+    <>
+      <Container>
+        <Box
+          display="flex"
+          direction="row"
+          justifyContent="space-between"
+          mb={2}
+        >
+          <Typography variant="h4">
+            Eventos
+          </Typography>
+          <Button variant="outline" onClick={handleClick}>
+            Mais {"\u2794"}
+          </Button>
+        </Box>
+      </Container>
+      <Grid container>
+        <Grid item md={8}>
+          <CustomCarrousel autoplay>
+            {data.slice(0, 10).map(e => {
+              return (
+                <SwiperSlide key={e.fileName}>
+                  <Slide {...e} />
+                </SwiperSlide>
+              );
+            })}
+          </CustomCarrousel>
+        </Grid>
+        <Grid item md={4}>
+          <Box height="auto" maxHeight="400px" width="100%" borderRadius={1}>
+            <CardActionArea >
+              <CardMedia
+                component="img"
+                src="/images/banner-aben.png"
+                sx={{
+                  position: "absolute",
+                  height: "400px",
+                  objectFit: "fill",
+                }}>
+              </CardMedia>
+              <Typography variant="h5" sx={{
+              }}>Oi! Sou a Aben-RS </Typography>
+            </CardActionArea>
+          </Box>
+        </Grid>
+      </Grid>
+
+
+
+
+
+
+
+    </>
   );
 };
 
