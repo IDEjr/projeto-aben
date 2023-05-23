e8770753130505e1705febaa1102902d5c4a5ce4
leoheisler <72279482+leoheisler@users.noreply.github.com>
2023-05-23 10:47:28 -0300

    issue#69
    Adição de um botão e correção dos estilos

--- src/components/AssocieseButton/index.js
+++ src/components/AssocieseButton/index.js
@@ -3,10 +3,13 @@ import styles from './Associese.module.css'; // Default styles
 function AssocieseButton(){
     return(
         <div className={styles.container}>
-            <a className={styles.button} href="https://web.eventogyn.com.br/event/abenrs/registration"> 
+            <a className={styles.button} href="https://web.eventogyn.com.br/event/abenrs/site/content/associe-se"> 
                 Associar / Renovar Anuidade
             </a>
-            <a className={styles.button} href="https://web.eventogyn.com.br/event/abenrs/attendee"> 
+            <a className={styles.button} href="https://web.eventogyn.com.br/event/abenrs/site/content/area-do-associado"> 
+               Emitir Recibo
+            </a>
+            <a className={styles.button} href="https://web.eventogyn.com.br/event/abenrs/site/content/area-do-associado"> 
                 Área do Associado
             </a>
         </div>
